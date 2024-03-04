71542c49106e9fa463561b76782a0e0c44593318
9oelM <hj923@hotmail.com>
2024-03-04 14:47:47 +0800

    refactor: revert back to window.Scaffolding

--- src/scaffolding/export.js
+++ src/scaffolding/export.js
@@ -1,7 +1,11 @@
 import * as Scaffolding from './scaffolding';
 
+if (window.Scaffolding) {
+  console.error('Scaffolding is already defined on the window. This may cause issues with the Scaffolding library.');
+}
+
 if (process.env.NODE_ENV !== 'production') {
   console.log('This is not a production build. Set NODE_ENV to production or use `npm run build-prod` for improved file size and performance (This message will go away).');
 }
 
-export default Scaffolding;
+window.Scaffolding = Scaffolding;
